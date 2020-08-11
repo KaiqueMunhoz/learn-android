1318867b98700d232c9f435d023243fb79c66e51
Kaique Munhoz <munhoz13km@gmail.com>
2020-08-11 20:56:17 -0300

    feat: Add a listView

--- alura/Agenda/app/src/main/java/com/learn/agenda/MainActivity.java
+++ alura/Agenda/app/src/main/java/com/learn/agenda/MainActivity.java
@@ -2,9 +2,14 @@ package com.learn.agenda;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends Activity {
 
@@ -12,5 +17,9 @@ public class MainActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ListView listaDeAlunos = findViewById(R.id.activity_main_list_view);
+        List<String> alunos = new ArrayList<>(Arrays.asList("Kaique", "Munhoz"));
+        listaDeAlunos.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos));
     }
 }
