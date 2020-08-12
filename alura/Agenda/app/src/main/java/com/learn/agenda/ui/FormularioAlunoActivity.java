4aedf11de75b0bc498529864c9360099d640a540
Kaique Munhoz <munhoz13km@gmail.com>
2020-08-11 21:50:11 -0300

    feat: Add a new activity

---
+++ alura/Agenda/app/src/main/java/com/learn/agenda/ui/FormularioAlunoActivity.java
@@ -0,0 +1,17 @@
+package com.learn.agenda.ui;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.learn.agenda.R;
+
+public class FormularioAlunoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_aluno);
+    }
+}
