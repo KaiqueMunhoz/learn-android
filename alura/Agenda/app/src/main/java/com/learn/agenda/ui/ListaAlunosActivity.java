7670ae735c7b34adc9ef784ab29495e2c1fdf4da
Kaique Munhoz <munhoz13km@gmail.com>
2020-08-11 21:52:24 -0300

    feat: Change activity_main name

--- alura/Agenda/app/src/main/java/com/learn/agenda/ui/ListaAlunosActivity.java
+++ alura/Agenda/app/src/main/java/com/learn/agenda/ui/ListaAlunosActivity.java
@@ -13,14 +13,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class ListaAlunosActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_lista_alunos);
         setTitle("Lista de alunos");
-        final ListView listaDeAlunos = findViewById(R.id.activity_main_list_view);
+        final ListView listaDeAlunos = findViewById(R.id.activity_lista_alunos_list_view);
         List<String> alunos = new ArrayList<>(Arrays.asList("Kaique", "Munhoz"));
         listaDeAlunos.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos));
     }
