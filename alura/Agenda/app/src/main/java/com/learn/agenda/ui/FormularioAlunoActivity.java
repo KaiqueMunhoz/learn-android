f0b133e8bed08501e82c7c4c9975e93b93ef6bc9
Kaique Munhoz <munhoz13km@gmail.com>
2020-08-11 22:19:42 -0300

    feat: Use the values on the EditTexts

--- alura/Agenda/app/src/main/java/com/learn/agenda/ui/FormularioAlunoActivity.java
+++ alura/Agenda/app/src/main/java/com/learn/agenda/ui/FormularioAlunoActivity.java
@@ -2,10 +2,14 @@ package com.learn.agenda.ui;
 
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.learn.agenda.R;
+import com.learn.agenda.model.Aluno;
 
 public class FormularioAlunoActivity extends AppCompatActivity {
 
@@ -13,5 +17,23 @@ public class FormularioAlunoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_aluno);
+
+        final EditText campoNome = findViewById(R.id.activity_formulario_aluno_nome);
+        final EditText campoTelefone = findViewById(R.id.activity_formulario_aluno_telefone);
+        final EditText campoEmail = findViewById(R.id.activity_formulario_aluno_email);
+
+        final View botao = findViewById(R.id.activity_formulario_aluno_botao_salvar);
+        botao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String nome = campoNome.getText().toString();
+                final String telefone = campoTelefone.getText().toString();
+                final String email = campoEmail.getText().toString();
+
+                final Aluno aluno = new Aluno(nome, telefone, email);
+                Toast.makeText(FormularioAlunoActivity.this, aluno.getNome() + " - " + aluno.getTelefone() + " - " + aluno.getEmail(), Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
