a0aacafb4bd61bb89cccb414cd8ce95f3c3cb896
Kaique Munhoz <munhoz13km@gmail.com>
2020-08-11 21:35:46 -0300

    feat: Change Activity to AppCompatActivity

--- alura/Agenda/app/src/main/java/com/learn/agenda/MainActivity.java
+++ alura/Agenda/app/src/main/java/com/learn/agenda/MainActivity.java
@@ -1,23 +1,23 @@
 package com.learn.agenda;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        setTitle("Lista de alunos");
         final ListView listaDeAlunos = findViewById(R.id.activity_main_list_view);
         List<String> alunos = new ArrayList<>(Arrays.asList("Kaique", "Munhoz"));
         listaDeAlunos.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos));
