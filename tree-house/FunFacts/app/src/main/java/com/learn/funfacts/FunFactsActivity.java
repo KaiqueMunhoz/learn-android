70ab901cb22491f2588e3eb4ce65b7d2033ad8c6
Kaique Munhoz <munhoz13km@gmail.com>
2021-01-04 21:27:35 -0300

    feat: Add random numbers into the factTextView

--- tree-house/FunFacts/app/src/main/java/com/learn/funfacts/FunFactsActivity.java
+++ tree-house/FunFacts/app/src/main/java/com/learn/funfacts/FunFactsActivity.java
@@ -3,12 +3,31 @@ package com.learn.funfacts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class FunFactsActivity extends AppCompatActivity {
+
+    private TextView factTextView;
+    private Button showFactButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fun_facts);
+
+        factTextView = findViewById(R.id.factTextView);
+        showFactButton = findViewById(R.id.showFactButton);
+
+        showFactButton.setOnClickListener(button -> {
+            final int randomNumber  = new Random().nextInt(3);
+            final String funFact = randomNumber + "";
+            factTextView.setText(funFact);
+        });
+
     }
 }
