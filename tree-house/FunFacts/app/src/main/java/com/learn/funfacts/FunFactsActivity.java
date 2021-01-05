f27a180bd7452f031c08f7ec649e8c213c19aefa
Kaique Munhoz <munhoz13km@gmail.com>
2021-01-04 21:34:59 -0300

    feat: Add a list of funFacts

--- tree-house/FunFacts/app/src/main/java/com/learn/funfacts/FunFactsActivity.java
+++ tree-house/FunFacts/app/src/main/java/com/learn/funfacts/FunFactsActivity.java
@@ -7,12 +7,27 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 
 public class FunFactsActivity extends AppCompatActivity {
 
     private TextView factTextView;
     private Button showFactButton;
+
+    final private ArrayList<String> facts = new ArrayList(Arrays.asList(
+            "Ants stretch when they wake up in the morning.",
+            "Ostriches can run faster than horses.",
+            "Olympic gold medals are actually made mostly of silver.",
+            "You are born with 300 bones; by the time you are an adult you will have 206.",
+            "It takes about 8 minutes for light from the Sun to reach Earth.",
+            "Some bamboo plants can grow almost a meter in just one day.",
+            "The state of Florida is bigger than England.",
+            "Some penguins can leap 2-3 meters out of the water.",
+            "On average, it takes 66 days to form a new habit.",
+            "Mammoths still walked the earth when the Great Pyramid was being built."
+    ));
 
 
     @Override
@@ -24,8 +39,8 @@ public class FunFactsActivity extends AppCompatActivity {
         showFactButton = findViewById(R.id.showFactButton);
 
         showFactButton.setOnClickListener(button -> {
-            final int randomNumber  = new Random().nextInt(3);
-            final String funFact = randomNumber + "";
+            final int randomNumber  = new Random().nextInt(facts.size());
+            final String funFact = facts.get(randomNumber);
             factTextView.setText(funFact);
         });
 
