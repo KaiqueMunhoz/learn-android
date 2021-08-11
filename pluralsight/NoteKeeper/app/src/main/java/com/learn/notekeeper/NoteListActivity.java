36875fe60ec39bceec2b3f1cf90bdc9c51015b19
Kaique Munhoz <munhoz13km@gmail.com>
2021-08-11 05:13:01 -0300

    feat: Add onClickItemListener to liistView

--- pluralsight/NoteKeeper/app/src/main/java/com/learn/notekeeper/NoteListActivity.java
+++ pluralsight/NoteKeeper/app/src/main/java/com/learn/notekeeper/NoteListActivity.java
@@ -2,7 +2,10 @@ package com.learn.notekeeper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -23,5 +26,9 @@ public class NoteListActivity extends AppCompatActivity {
         final ArrayAdapter<NoteInfo>  adapterNotes = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listOfNotes);
 
         listNotes.setAdapter(adapterNotes);
+        listNotes.setOnItemClickListener((parent, view, position, id) -> {
+            final Intent intent = new Intent(NoteListActivity.this, NoteActivity.class);
+            startActivity(intent);
+        });
     }
 }
