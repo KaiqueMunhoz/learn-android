d4ca068339cc81d174169f37688f38cf6482bc24
Kaique Munhoz <munhoz13km@gmail.com>
2021-08-11 05:02:12 -0300

    feat: Create initializeDisplayContent
    listView adapter

--- pluralsight/NoteKeeper/app/src/main/java/com/learn/notekeeper/NoteListActivity.java
+++ pluralsight/NoteKeeper/app/src/main/java/com/learn/notekeeper/NoteListActivity.java
@@ -3,6 +3,10 @@ package com.learn.notekeeper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class NoteListActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class NoteListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_list);
+        initializeDisplayContent();
+    }
+
+    private void initializeDisplayContent() {
+        final ListView listNotes = findViewById(R.id.list_notes);
+        final List<NoteInfo> listOfNotes = DataManager.getInstance().getNotes();
+        final ArrayAdapter<NoteInfo>  adapterNotes = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listOfNotes);
+
+        listNotes.setAdapter(adapterNotes);
     }
 }
